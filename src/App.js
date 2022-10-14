4da5818c8ebaa2f411b214709c1d188c1756f30e
jverarusso <wordart616@gmail.com>
2022-10-14 16:14:28 -0500

    creacion de componente

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Formulario></Formulario> 
         <h1>Hola mundo</h1>
       </header>
     </div>
