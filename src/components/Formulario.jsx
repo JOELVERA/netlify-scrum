abaf8d8762a5c43578d459deb4336ad49ca23e6b
jverarusso <wordart616@gmail.com>
2022-10-14 17:29:51 -0500

    mi formulario validado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,17 +4,20 @@ import { collection, onSnapshot, addDoc, doc, deleteDoc} from 'firebase/firestor
 
 
 const Formulario = () => {
-    const [fruta, setFruta] = useState('');
-    const [descripcion, setDescripcion] = useState('');
-    const [listaFrutas, setListaFrutas] = useState([])
+    const [nombre, setNombre] = useState('');
+    const [nit, setNit] = useState('');
+    const [tproductos, setTproductos] = useState('');
+    const [ciudad, setCiudad] = useState('');
+    const [email, setEmail] = useState('');
+    const [lista, setLista] = useState([])
 
 
     useEffect(() => {
 
         const obtenerDatos = async () => {    
           try {
-            await onSnapshot(collection(db, "frutas"), (querySnapshot) => {    
-              setListaFrutas(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+            await onSnapshot(collection(db, "proveedores"), (querySnapshot) => {    
+              setLista(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
             });
           } catch (error) {
             console.log(error);
@@ -27,36 +30,59 @@ const Formulario = () => {
     const eliminar = async id => {
         console.log(id)
         try{
-            await deleteDoc(doc(db,'frutas',id))
+            await deleteDoc(doc(db,'proveedores',id))
         }catch(error){
             console.log(error)
         }
     }
 
-    const guardarFrutas = async (e) =>{
+    const guardar = async (e) =>{
         e.preventDefault()
-        if(!fruta.trim()){
-            alert('Ingrese la fruta')
+        if(!nombre.trim()){
+            alert('Ingrese el nombre')
             return
         }
-        if(!descripcion.trim()){
-            alert('Ingrese la descripcion')
+        if(!nit.trim()){
+            alert('Ingrese el Nit')
+            return
+        }
+        if(!tproductos.trim()){
+            alert('Ingrese el Tipo de producto')
+            return
+        }
+        if(!ciudad.trim()){
+            alert('Ingrese la ciudad ')
+            return
+        }
+        if(!email.trim()){
+            alert('Ingrese el email')
             return
         }
         try{
 
-            const data = await addDoc(collection(db,'frutas'),{
-                nombreFruta: fruta,
-                nombreDescripcion: descripcion
+            const data = await addDoc(collection(db,'proveedores'),{
+                nombre: nombre,
+                nit: nit,
+                tproductos: tproductos,
+                ciudad: ciudad,
+                email: email
             })
 
-            setListaFrutas([
-                ...listaFrutas,
-                {nombreFruta:fruta, nombreDescripcion:descripcion, id:data.id}
+            setLista([
+                ...lista,
+                {nombre:nombre,
+                nit:nit,
+                tproductos: tproductos,
+                ciudad: ciudad,
+                email: email,
+                id:data.id}
             ])
 
-            setFruta('')
-            setDescripcion('')
+            setNombre('')
+            setNit('')
+            setTproductos('')
+            setCiudad('')
+            setEmail('')
             e.target.reset()
         }catch(error){
             console.log(error)
@@ -65,16 +91,16 @@ const Formulario = () => {
 
   return (
     <div className='container mt-5'>
-        <h1 className='text-center'>CRUD BÁSICO</h1>
+        <h1 className='text-center'>PROVEEDORES</h1>
         <hr/>
         <div className='row'>
             <div className="col-8">
-                <h4 className="text-center">Listado de Frutas</h4>
+                <h4 className="text-center">Listado de Proveedores</h4>
                 <ul className="list-group">
                     {
-                        listaFrutas.map(item =>(
+                        lista.map(item =>(
                             <li className='list-group-item' key={item.id}>
-                                <span className='lead'>{item.nombreFruta}-{item.nombreDescripcion}</span>
+                                <span className='lead'>{item.nombre}    -    {item.nit}    -    {item.tproductos}    -    {item.ciudad}    -    {item.email}</span>
                                 <button className='btn btn-danger btn-sm float-end mx-2' onClick={()=>eliminar(item.id)}>Eliminar</button>
                             </li>
                         ))
@@ -84,22 +110,40 @@ const Formulario = () => {
         
         <div className='col-4'>
             <h4 className="text-center">
-                Agregar Frutas
+                Agregar Nuevo Proveedor
             </h4>
-            <form onSubmit={guardarFrutas}>
+            <form onSubmit={guardar}>
                 <input 
                 className='form-control mb-2'
                 type="text" 
-                placeholder='Ingrese Fruta'
-                onChange={(e)=>setFruta(e.target.value)}
-                value = {fruta}
+                placeholder='Ingrese Nombre del Proveedor'
+                onChange={(e)=>setNombre(e.target.value)}
+                value = {nombre}
                 ></input>
                 <input 
                 className='form-control mb-2'
+                type="number" 
+                placeholder='Ingrese el nit del proveedor'
+                onChange={(e) => setNit(e.target.value)}
+                value = {nit}></input>
+                <input 
+                className='form-control mb-2'
                 type="text" 
-                placeholder='Ingrese Descripción'
-                onChange={(e) => setDescripcion(e.target.value)}
-                value = {descripcion}></input>
+                placeholder='Ingrese el Tipo del productos'
+                onChange={(e) => setTproductos(e.target.value)}
+                value = {tproductos}></input>
+                <input 
+                className='form-control mb-2'
+                type="text" 
+                placeholder='Ingrese la cuidad del proveedor'
+                onChange={(e) => setCiudad(e.target.value)}
+                value = {ciudad}></input>
+                <input 
+                className='form-control mb-2'
+                type="email" 
+                placeholder='Ingrese el Email de contacto'
+                onChange={(e) => setEmail(e.target.value)}
+                value = {email}></input>
                 <button 
                 className='btn btn-primary btn-block'
                 type='submit'
